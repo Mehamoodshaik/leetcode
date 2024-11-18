97b11c86b0a68be9a0fe393dc8dd8214894ed109
Mehamood Shaik <54311158+Mehamoodshaik@users.noreply.github.com>
2024-11-18 09:32:58 -0600

    Update 1652. Defuse the Bomb.java
    Added Comments for better Understanding

--- 1652. Defuse the Bomb.java
+++ 1652. Defuse the Bomb.java
@@ -1,21 +1,30 @@
 class Solution {
     public int[] decrypt(int[] code, int k) {
+
+        //Initialize Variables 
         int n = code.length, windowSum = 0;
         int[] res = new int[n];
+
+        //If value of k is zero return res
         if(k==0){
             return res;
         }
 
+        //Initialize the window pointers 
         int left =1,right = k;
+
+        //If the vlaue of k is negative, then change the pointers
         if(k<0){
             left = n- Math.abs(k);
             right = n-1;
         }
 
+        //Calculate the window Sum
         for(int i=left;i<=right;i++){
             windowSum += code[i];
         }
 
+        // Now slide the window by iterating the values in code
         for(int i=0;i<n;i++){
             res[i] = windowSum;
             windowSum -= code[left%n];
