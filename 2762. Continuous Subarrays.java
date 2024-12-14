2c60e794c5ac6877fbd55f1f2d76aa707400723d
Mehamood Shaik <54311158+Mehamoodshaik@users.noreply.github.com>
2024-12-14 15:21:04 -0600

    Create 2762. Continuous Subarrays.java

---
+++ 2762. Continuous Subarrays.java
@@ -0,0 +1,24 @@
+class Solution {
+    public long continuousSubarrays(int[] nums) {
+        TreeMap<Integer,Integer> map = new TreeMap<>();
+        int left=0,right=0;
+        long count = 0;
+
+        int n = nums.length;
+        while(right<n){
+            map.put(nums[right],map.getOrDefault(nums[right],0)+1);
+
+
+            while(map.lastEntry().getKey()-map.firstEntry().getKey()>2){
+                map.put(nums[left],map.get(nums[left])-1);
+                if(map.get(nums[left])==0){
+                    map.remove(nums[left]);
+                }
+                left++;
+            }
+            count+=right-left+1;
+            right++;
+        }
+        return count;
+    }
+}
